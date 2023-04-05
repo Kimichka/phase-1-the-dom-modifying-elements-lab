ade3b170056741f1a24673f34edf86bceb98901f
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-04-05 12:59:28 -0400

    lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+//Old code from phase 1:
+//const main = document.getElementById('main');
+//main.remove();
+//const newHeader = document.createElement('h1');
+//newHeader.setAttribute('id','victory');
+//newHeader.innerHTML = "KIMMI is the champion";
+//document.head.appendChild(element);
+
+
+
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "KIMMI is the champion";
+document.body.appendChild(newHeader);
